f3536ebc960cce3f0c6279239f5dae70230b95e5
sammun1926 <munorusam10@gmail.com>
2023-03-22 12:42:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById("main")
+element.remove();
+
+var newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Paps  is the champion"
